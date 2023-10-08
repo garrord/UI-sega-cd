6a8a154082836263d6e6b210e24317ecee22b194
Ryan Garro <rg171503@gmail.com>
2023-10-07 21:21:13 -0400

    did shit

--- src/app/components/variants/variants.component.ts
+++ src/app/components/variants/variants.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { SafeUrl } from "@angular/platform-browser";
 import { UIVariantModel } from "src/app/models/ui-variant.model";
 
 @Component({
@@ -9,8 +10,36 @@ import { UIVariantModel } from "src/app/models/ui-variant.model";
 
 export class VariantsComponent {
     @Input() uiVariants!: UIVariantModel;
+    tempLinkedModel: TempModel[] = [];
+    isComplete:boolean = false;
 
     ngOnInit(): void {
-        this.uiVariants;
+        let index = 0;
+        this.uiVariants.uiLinkedVariantModel.forEach(x => {
+            let variant = new TempModel();
+            variant.description = x.description;
+            if (x.discVariantImage){
+                variant.images.push(x.discVariantImage!);
+            }
+            if (x.manualVariantImage){
+                variant.images.push(x.manualVariantImage!);
+            }
+            if (x.frontBoxVariantImage){
+                variant.images.push(x.frontBoxVariantImage!);
+            }
+            if (x.backBoxVariantImage){
+                variant.images.push(x.backBoxVariantImage!);
+            }
+            this.tempLinkedModel.push(variant);
+            index++;
+        });
+        this.isComplete = true;
     }
+}
+
+
+//creating temp model to easily display the linked variants at time until
+export class TempModel {
+    description?:string;
+    images: SafeUrl[] = [];
 }
