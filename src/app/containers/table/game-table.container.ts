e35b63f6890da1e4fdbf7ff2b377ceb6139d59bf
Ryan Garro <rg171503@gmail.com>
2023-10-08 20:28:01 -0400

    caching might be working?? using sessionStorage; will look into JS caching api

--- src/app/containers/table/game-table.container.ts
+++ src/app/containers/table/game-table.container.ts
@@ -18,20 +18,28 @@ export class GameTableContainer implements OnInit, OnDestroy{
     session:string = "games";
 
     ngOnInit(): void {
-        if (sessionStorage.getItem('games') == 'true'){
-            console.log("session data!");
+        // if (sessionStorage.getItem('games') == 'true'){
+        //     console.log("session data!");
+        // }
+        if (sessionStorage['games']){
+            console.log("session data! part 2");
+            this.games = JSON.parse(sessionStorage['games']);
+            this.isComplete = true;
         }
-        this.allGamesSubscription = this.gameService.getAllGames().subscribe({
-            next: (x) => this.games = x,
-            error: (x) => console.log(x),
-            complete: () => {
-                sessionStorage.setItem('games', 'true');
-                this.isComplete = true
-            }
-        });
+        else{
+            this.allGamesSubscription = this.gameService.getAllGames().subscribe({
+                next: (x) => this.games = x,
+                error: (x) => console.log(x),
+                complete: () => {
+                    //let isCacheSupported = 'caches' in window; => need to look more into this; 
+                    sessionStorage.setItem('games', JSON.stringify(this.games));
+                    this.isComplete = true
+                }
+            });
+        }
     }
 
     ngOnDestroy(): void {
-        this.allGamesSubscription.unsubscribe();
+        this.allGamesSubscription?.unsubscribe();
     }
 }
